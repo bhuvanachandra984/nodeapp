2f82339b121f971563ae20ffb5daa8982a40ef81
BhuvanChandra86 <118716532+BhuvanChandra86@users.noreply.github.com>
2023-05-05 11:28:57 +0530

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+const adminRoutes = require("./admin");
+router.get("/", (req, res, next) => {
+  console.log(adminRoutes.products);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+
+module.exports = router;
